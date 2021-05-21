e4b4541ed4638e7e05b299b3cd59a31ca9444bef
TAiGA <@>
2021-05-21 15:26:16 +0800

    Add server mode

--- ShaderCompiler.c
+++ ShaderCompiler.c
@@ -1,6 +1,11 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
 
 #include <EGL/egl.h>
 #include <GLES2/gl2.h>
@@ -193,6 +198,163 @@ bool shaderCompile(int shaderVersion, GLenum shaderType, const char* shaderCode,
   return succeed;
 }
 
+struct ClientData
+{
+  int socket;
+  int shaderType;
+  int shaderSize;
+  int shaderRead;
+  char* shaderCode;
+  int binarySize;
+  int binaryWrite;
+  char* binaryCode;
+};
+
+bool serve(const char* ip, unsigned short port)
+{
+  int epoll = epoll_create(1024);
+  if (epoll < 0)
+  {
+    printf("%s is failed\n", "epoll_create");
+    return false;
+  }
+
+  struct sockaddr_in sa;
+  sa.sin_family = AF_INET;
+  sa.sin_port = htons(port);
+  inet_pton(AF_INET, ip, &sa.sin_addr);
+  int server = socket(sa.sin_family, SOCK_STREAM, IPPROTO_TCP);
+  if (server < 0)
+  {
+    printf("%s is failed\n", "socket");
+    return false;
+  }
+
+  if (bind(server, (struct sockaddr*)&sa, sizeof(sa)) < 0)
+  {
+    printf("%s is failed\n", "bind");
+    return false;
+  }
+
+  if (listen(server, 512) < 0)
+  {
+    printf("%s is failed\n", "listen");
+    return false;
+  }
+  
+  struct epoll_event ev;
+  ev.events = EPOLLIN;
+  ev.data.ptr = NULL;
+  epoll_ctl(epoll, EPOLL_CTL_ADD, server, &ev);
+
+  struct epoll_event events[1024];
+  for (;;)
+  {
+    int count = epoll_wait(epoll, events, 1024, -1);
+    if (count <= 0)
+      break;
+    printf("%s : %d\n", "epoll_wait", count);
+
+    for (int i = 0; i < count; ++i)
+    {
+      struct ClientData* data = events[i].data.ptr;
+      if (data == NULL)
+      {
+	struct sockaddr_storage addr;
+        socklen_t addrlen = sizeof(addr);
+	int client = accept(server, (struct sockaddr*)&addr, &addrlen);
+	if (client < 0)
+	  continue;
+	printf("%s : %d\n", "accept", client);
+
+	struct ClientData* data = calloc(sizeof(struct ClientData), 1);
+	if (data == NULL)
+	  continue;
+	data->socket = client;
+	
+	struct epoll_event ev;
+	ev.events = EPOLLIN | EPOLLOUT | EPOLLERR | EPOLLET;
+	ev.data.ptr = data;
+	epoll_ctl(epoll, EPOLL_CTL_ADD, client, &ev);
+	continue;
+      }
+      if (events[i].events & EPOLLIN)
+      {
+	if (data->shaderSize == 0)
+	{
+	  int header[3];
+	  if (recv(data->socket, header, sizeof(header), MSG_DONTWAIT | MSG_NOSIGNAL) <= 0)
+	    continue;
+	  if (header[0] != 0xDEADBEEF || header[2] > 1048576)
+	  {
+	    events[i].events |= EPOLLERR;
+	  }
+	  else
+	  {
+	    data->shaderType = header[1];
+	    data->shaderSize = header[2];
+	    data->shaderCode = calloc(data->shaderSize, 1);
+	  }
+	}
+	if (data->shaderRead != data->shaderSize)
+	{
+	  int read = recv(data->socket, data->shaderCode + data->shaderRead, data->shaderSize - data->shaderRead, MSG_DONTWAIT | MSG_NOSIGNAL);
+	  printf("%s : %d\n", "recv", read);
+	  if (read <= 0)
+	    continue;
+	  data->shaderRead += read;
+	  if (data->shaderRead != data->shaderSize)
+	    continue;
+	  
+	  void* binaryCode = NULL;
+	  int binarySize = 0;
+	  shaderCompile(100, data->shaderType, data->shaderCode, data->shaderSize, &binaryCode, &binarySize);
+	  if (binaryCode == NULL)
+	  {
+	    events[i].events |= EPOLLERR;
+	  }
+	  else
+	  {
+	    data->binarySize = binarySize;
+	    data->binaryCode = binaryCode;
+	    if (send(data->socket, &binarySize, sizeof(binarySize), MSG_DONTWAIT | MSG_NOSIGNAL) <= 0)
+	    {
+	      events[i].events |= EPOLLERR;
+	    }
+	    else
+	    {
+	      events[i].events |= EPOLLOUT;
+	    }
+	  }
+	}
+      }
+      if (events[i].events & EPOLLOUT)
+      {
+	if (data->binaryCode && data->binaryWrite != data->binarySize)
+        {
+	  int write = send(data->socket, data->binaryCode + data->binaryWrite, data->binarySize - data->binaryWrite, MSG_DONTWAIT | MSG_NOSIGNAL);
+	  printf("%s : %d\n", "send", write);
+	  if (write <= 0)
+	    continue;
+	  data->binaryWrite += write;
+	  if (data->binaryWrite != data->binarySize)
+	    continue;
+	  events[i].events |= EPOLLERR;
+	}
+      }      
+      if (events[i].events & EPOLLERR)
+      {
+	close(data->socket);
+	printf("%s : %d\n", "close", data->socket);
+
+	epoll_ctl(epoll, EPOLL_CTL_DEL, data->socket, NULL);
+      }
+    }
+  }
+  
+  return true;
+}
+
 int main(int argc, char** argv)
 {
   if (eglCreate() == false)
@@ -200,7 +362,11 @@ int main(int argc, char** argv)
     return 0;
   }
 
-  if (argc >= 2 && (strstr(argv[1], ".vert") || strstr(argv[1], ".frag")))
+  if (argc >= 4 && strcmp(argv[1], "-s") == 0)
+  {
+    serve(argv[2], atoi(argv[3]));
+  }
+  else if (argc >= 2 && (strstr(argv[1], ".vert") || strstr(argv[1], ".frag")))
   {
     FILE* file = fopen(argv[1], "rb");
     if (file == NULL)
